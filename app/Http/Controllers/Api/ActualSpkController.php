1563c0cda8b1d55b7f50279622283b21aaaab00e
Pramtoxz <pramuditometra@gmail.com>
2026-06-05 15:16:21 +0700

    feat: actual SPK

--- app/Http/Controllers/Api/ActualSpkController.php
+++ app/Http/Controllers/Api/ActualSpkController.php
@@ -34,25 +34,30 @@ class ActualSpkController extends Controller
                 'spk.IDCustomer',
                 'mastercustomer.NamaCustomer',
                 'mastercustomer.NoHp',
+                'mastercustomer.Alamat',
                 'spk.IDJenisPembayaran',
                 'setupjenispembayaran.JenisPembayaran',
                 'spk.NamaLeasing',
-                DB::raw("\"SpkDetail\".\"fk_tipe\" || '-' || \"SpkDetail\".\"fk_warna\" as tipe"),
-                'mastergroupsegmenmotor.DeskripsiType as nama_tipe',
-                'tblwarna.warna as nama_warna',
-                'SpkDetail.harga_unit',
-                'SpkDetail.diskon_unit',
+                'spk.NamaKaryawan',
+                'spk.IDJenisPembelian',
+                'SetupTipeCustomer.jenis_pembelian',
+                'spk.flag_bundling_acc',
+                'spk.is_bundling_new',
+                'spk.is_bundling_fixed',
                 'spk.DP',
                 'spk.Cicilan',
                 'spk.Tenor',
                 'spk.status_spk',
+                'salesorder.status_so',
                 'spk.created_at'
             )
             ->leftJoin('H1_DOS.mastercustomer', 'mastercustomer.IDCustomer', '=', 'spk.IDCustomer')
-            ->leftJoin('H1_DOS.SpkDetail', 'SpkDetail.IdSPK', '=', 'spk.IDSpk')
             ->leftJoin('H1_DOS.setupjenispembayaran', 'setupjenispembayaran.IDJenisPembayaran', '=', 'spk.IDJenisPembayaran')
-            ->leftJoin('H1_DOS.mastergroupsegmenmotor', 'mastergroupsegmenmotor.KodeType', '=', 'SpkDetail.fk_tipe')
-            ->leftJoin('public.tblwarna', 'tblwarna.kd_warna', '=', 'SpkDetail.fk_warna')
+            ->leftJoin('Master_Schema.SetupTipeCustomer', 'SetupTipeCustomer.id_jenis', '=', 'spk.IDJenisPembelian')
+            ->leftJoin('H1_DOS.salesorder', function ($join) {
+                $join->on('salesorder.IDSPK', '=', 'spk.IDSpk')
+                     ->where('salesorder.status_so', '!=', 'b');
+            })
             ->where('spk.id_flp', $flp->id_flp)
             ->orderBy('spk.TglSPK', 'desc');
 
@@ -106,32 +111,70 @@ class ActualSpkController extends Controller
         $spk = DB::connection('pgsql_nms')
             ->table('H1_DOS.spk')
             ->select(
+                // SPK header
                 'spk.IDSpk',
+                'spk.IDGuestBook',
                 'spk.TglSPK',
+                'spk.Tgl_Pengiriman',
                 'spk.IDCustomer',
-                'mastercustomer.NamaCustomer',
-                'mastercustomer.NoHp',
-                'mastercustomer.Alamat',
+                'spk.IDIndent',
+                'spk.STNKBPKB_atas_nama',
+                'spk.nofakturpajak',
+                'spk.nm_penanggung_jawab',
+                'spk.alamat',
+                'spk.JenisRO',
+                'spk.IDTipeCustomer',
                 'spk.IDJenisPembayaran',
                 'setupjenispembayaran.JenisPembayaran',
+                'spk.IDJenisPembelian',
+                'SetupTipeCustomer.jenis_pembelian',
+                'spk.NamaKaryawan',
                 'spk.NamaLeasing',
-                DB::raw("\"SpkDetail\".\"fk_tipe\" || '-' || \"SpkDetail\".\"fk_warna\" as tipe"),
-                'mastergroupsegmenmotor.DeskripsiType as nama_tipe',
-                'tblwarna.warna as nama_warna',
-                'SpkDetail.harga_unit',
-                'SpkDetail.diskon_unit',
+                'spk.IdLeasing',
+                'spk.IDAktifitasPenjualan',
+                'spk.tipe_source',
+                'spk.IDActivity',
+                // Pembayaran
                 'spk.DP',
+                'spk.DpBayar',
+                'spk.TandaJadi',
                 'spk.Cicilan',
                 'spk.Tenor',
+                'spk.PersenDPPricelist',
+                'spk.PersenDPBayar',
+                // Flag
+                'spk.FakturPajak',
+                'spk.flag_plat_merah',
+                'spk.flag_prog_pemerintah',
+                'spk.flag_bundling_acc',
+                'spk.is_bundling_new',
+                'spk.is_bundling_fixed',
+                // Status
                 'spk.status_spk',
+                'salesorder.status_so',
+                // Customer
+                'mastercustomer.NamaCustomer',
+                'mastercustomer.NoKTP',
+                'mastercustomer.IDKartuKeluarga',
+                'mastercustomer.Alamat',
+                'mastercustomer.RTRW',
+                'mastercustomer.KodePos',
+                'mastercustomer.kode_area',
+                'mastercustomer.NoTelp',
+                'mastercustomer.NoHp',
+                'mastercustomer.NoHp2',
+                'mastercustomer.Email',
+                // Timestamps
                 'spk.created_at',
                 'spk.updated_at'
             )
             ->leftJoin('H1_DOS.mastercustomer', 'mastercustomer.IDCustomer', '=', 'spk.IDCustomer')
-            ->leftJoin('H1_DOS.SpkDetail', 'SpkDetail.IdSPK', '=', 'spk.IDSpk')
             ->leftJoin('H1_DOS.setupjenispembayaran', 'setupjenispembayaran.IDJenisPembayaran', '=', 'spk.IDJenisPembayaran')
-            ->leftJoin('H1_DOS.mastergroupsegmenmotor', 'mastergroupsegmenmotor.KodeType', '=', 'SpkDetail.fk_tipe')
-            ->leftJoin('public.tblwarna', 'tblwarna.kd_warna', '=', 'SpkDetail.fk_warna')
+            ->leftJoin('Master_Schema.SetupTipeCustomer', 'SetupTipeCustomer.id_jenis', '=', 'spk.IDJenisPembelian')
+            ->leftJoin('H1_DOS.salesorder', function ($join) {
+                $join->on('salesorder.IDSPK', '=', 'spk.IDSpk')
+                     ->where('salesorder.status_so', '!=', 'b');
+            })
             ->where('spk.IDSpk', $id)
             ->where('spk.id_flp', $flp->id_flp)
             ->first();
@@ -143,9 +186,26 @@ class ActualSpkController extends Controller
             ], 404);
         }
 
+        $units = DB::connection('pgsql_nms')
+            ->table('H1_DOS.SpkDetail')
+            ->select(
+                DB::raw("\"SpkDetail\".\"fk_tipe\" || '-' || \"SpkDetail\".\"fk_warna\" as tipe"),
+                'mastergroupsegmenmotor.DeskripsiType as nama_tipe',
+                'tblwarna.warna as nama_warna',
+                'SpkDetail.fk_tipe',
+                'SpkDetail.fk_warna',
+                'SpkDetail.harga_unit',
+                'SpkDetail.diskon_unit',
+                'SpkDetail.qty_unit'
+            )
+            ->leftJoin('H1_DOS.mastergroupsegmenmotor', 'mastergroupsegmenmotor.KodeType', '=', 'SpkDetail.fk_tipe')
+            ->leftJoin('public.tblwarna', 'tblwarna.kd_warna', '=', 'SpkDetail.fk_warna')
+            ->where('SpkDetail.IdSPK', $id)
+            ->get();
+
         return response()->json([
             'success' => true,
-            'data' => $spk,
+            'data' => array_merge((array) $spk, ['units' => $units]),
         ]);
     }
 }
